21c015a44d1077d8600832c63ae43a25916044fc
user <user@lenovo>
2016-11-28 23:41:43 +0700

    Kalkulaor_sederhana

---
+++ src/display.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+class display {
+
+    static void setText(String angka) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    
+}
---
+++ src/uts_11140367_kalkulator_sederhana/UTS_11140367_Kalkulator_Sederhana.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts_11140367_kalkulator_sederhana;
+
+/**
+ *
+ * @author user
+ */
+public class UTS_11140367_Kalkulator_Sederhana {
+
+   
+  
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
